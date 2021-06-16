a5398aecad31df0571861dd29509a4de8d06be71
DaNaRim <rangar.danarim@gmail.com>
2021-06-16 21:21:50 +0300

    project-lesson8

--- project/js/main.js
+++ project/js/main.js
@@ -15,4 +15,11 @@ window.addEventListener("DOMContentLoaded", () => {
             }
         });
     });
+
+    $('.carousel').slick({
+        autoplay: true,
+        autoplaySpeed: 10000,
+        prevArrow: '<div class="carousel_arrow carousel_prev"><i class="fas fa-chevron-left"></i></div>',
+        nextArrow: '<div class="carousel_arrow carousel_next"><i class="fas fa-chevron-right"></i></div>'
+    });
 });
